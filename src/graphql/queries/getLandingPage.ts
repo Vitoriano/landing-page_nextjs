f6688add369784f04988635519ad75495109096a
Vitoriano <vitorianoernandes2012@gmail.com>
2021-02-18 16:03:00 -0300

    Initial commit

---
+++ src/graphql/queries/getLandingPage.ts
@@ -0,0 +1,147 @@
+const GET_LANDING_PAGE = /* GraphQL */ `
+  fragment logo on LandingPage {
+    logo {
+      alternativeText
+      url
+    }
+  }
+
+  fragment header on LandingPage {
+    header {
+      title
+      description
+      button {
+        label
+        url
+      }
+      image {
+        alternativeText
+        url
+      }
+    }
+  }
+
+  fragment sectionAboutProject on LandingPage {
+    sectionAboutProject {
+      title
+      description
+      media {
+        alternativeText
+        url
+      }
+    }
+  }
+
+  fragment sectionTech on LandingPage {
+    sectionTech {
+      ttitle
+      techIcons {
+        title
+        icon {
+          url
+        }
+      }
+    }
+  }
+
+  fragment sectionConcepts on LandingPage {
+    sectionConcepts {
+      title
+      concepts {
+        title
+      }
+    }
+  }
+
+  fragment sectionModules on LandingPage {
+    sectionModules {
+      title
+      modules {
+        title
+        subtitle
+        description
+      }
+    }
+  }
+
+  fragment sectionAgenda on LandingPage {
+    sectionAgenda {
+      title
+      description
+    }
+  }
+
+  fragment pricingBox on LandingPage {
+    priceBox {
+      totalPrice
+      numberInstallments
+      priceInstallment
+      benefits
+      button {
+        label
+        url
+      }
+    }
+  }
+
+  fragment sectionAboutUs on LandingPage {
+    sectionAboutUs {
+      title
+      authors {
+        socialLinks {
+          url
+          title
+        }
+        photo {
+          alternativeText
+          url
+        }
+        name
+        role
+
+        description
+      }
+    }
+  }
+
+  fragment sectionReviews on LandingPage {
+    sectionReviews {
+      title
+      reviews {
+        name
+        text
+        photo {
+          url
+        }
+      }
+    }
+  }
+
+  fragment sectionFaq on LandingPage {
+    sectionFaq {
+      title
+      questions {
+        question
+        answer
+      }
+    }
+  }
+
+  query GET_LANDING_PAGE {
+    landingPage {
+      ...logo
+      ...header
+      ...sectionAboutProject
+      ...sectionTech
+      ...sectionConcepts
+      ...sectionModules
+      ...sectionAgenda
+      ...pricingBox
+      ...sectionAboutUs
+      ...sectionReviews
+      ...sectionFaq
+    }
+  }
+`
+
+export default GET_LANDING_PAGE
